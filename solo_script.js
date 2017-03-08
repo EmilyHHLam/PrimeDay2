740ee4b97cc3954f2b1da5f40ea3201e48d31da7
EmilyH <EmilyHHLam@gmail.com>
2017-03-08 15:55:50 -0600

    added if statement if the bonus gets negative

--- solo_script.js
+++ solo_script.js
@@ -21,82 +21,84 @@ position = document.getElementById('content');
 //console.log('before loop array' + array);
 var calcArray = [];
 function newCalFunc() {
-for(var i = 0; i < array.length; i++){
-	//scott's code FAULT
-	//array[i] = calculateSTI(array);
-	calcArray.push(calculateSTI(array[i]));
-}
-return calcArray;
+	for(var i = 0; i < array.length; i++){
+		//scott's code FAULT
+		//array[i] = calculateSTI(array);
+		calcArray.push(calculateSTI(array[i]));
+	}
+	return calcArray;
 }
 var newSetArray = newCalFunc();
 
 //Loop to get the new array shown on the html page
 for (var j = 0; j< newSetArray.length; j++) {
- newEl = document.createElement('li');
- newText = document.createTextNode(newSetArray[j]);
- newEl.appendChild(newText);
-position.appendChild(newEl);
+	newEl = document.createElement('li');
+	newText = document.createTextNode(newSetArray[j]);
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
 }
 
 function calculateSTI(array){
-  var newArray = [];
+	var newArray = [];
 
-  newArray[0] = array[0]; // employee name
-  var employeeNumber = array[1];
-  var baseSalary = array[2];
-  var reviewScore = array[3];
+	newArray[0] = array[0]; // employee name
+	var employeeNumber = array[1];
+	var baseSalary = array[2];
+	var reviewScore = array[3];
 
-  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
-  if(bonus > 0.13){
-    bonus = 0.13;
-  }
-//Round up decimal data'
+	var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+	if(bonus > 0.13){
+		bonus = 0.13;
+	} else if (bonus < 0) {
+		bonus = 0;
+	}
+	//Round up decimal data'
 	//FAULT - not round the bonus;
 	//Scott's code: newArray[1] = Math.round(bonus);
-  newArray[1] = bonus; // percentage of STI employee to receive
-  newArray[2] = Math.round(baseSalary * (1.0 + bonus)); // adjusted annual compensation
-  newArray[3] = Math.round(baseSalary * bonus); //total bonus rounded to nearest dollar
-  console.log(newArray[0] + " " + Math.round(newArray[1]) + " " + Math.round(newArray[2]) + " " + Math.round(newArray[3]));
-  return newArray;
+	newArray[1] = bonus; // percentage of STI employee to receive
+	newArray[2] = Math.round(baseSalary * (1.0 + bonus)); // adjusted annual compensation
+	newArray[3] = Math.round(baseSalary * bonus); //total bonus rounded to nearest dollar
+	console.log(newArray[0] + " " + newArray[1] + " " + Math.round(newArray[2]) + " " + Math.round(newArray[3]));
+	return newArray;
 }
 
 function getBaseSTI(reviewScore){
-  var basePercent;
-  switch(reviewScore){
-    case 1:
-      basePercent = 0;
-      break;
-    case 2:
-      basePercent = 0;
-      break;
-    case 3:
-      basePercent = 0.04;
-      break;
-    case 4:
-      basePercent = 0.06;
-      break;
-    case 5:
-      basePercent = 0.10;
-      break;
-  }
+	var basePercent;
+	switch(reviewScore){
+		case 1:
+		basePercent = 0;
+		break;
+		case 2:
+		basePercent = 0;
+		break;
+		case 3:
+		basePercent = 0.04;
+		break;
+		case 4:
+		basePercent = 0.06;
+		break;
+		case 5:
+		basePercent = 0.10;
+		break;
+	}
 	//takeout basePercent -1
-	//console.log('base%' + basePercent);
-  return basePercent;
+console.log('base%' + basePercent);
+	return basePercent;
 }
 
 function getYearAdjustment(employeeNumber){
-  var yearAdjustment = 0;
-  if(employeeNumber.length == 4){
-    yearAdjustment = 0.05;
-  }
-  return yearAdjustment;
+	var yearAdjustment = 0;
+	if(employeeNumber.length == 4){
+		yearAdjustment = 0.05;
+	}
+	return yearAdjustment;
 }
 
 function getIncomeAdjustment(salary){
-  var incomeAdjustment = 0;
-  salary = parseInt(salary);
-  if(salary > 65000){
-    incomeAdjustment = 0.01;
-  }
-  return incomeAdjustment;
+	var incomeAdjustment = 0;
+	salary = parseInt(salary);
+	if(salary > 65000){
+		incomeAdjustment = 0.01;
+	}
+	return incomeAdjustment;
 }
